47604583bf26af396e3065effaa59f11a3b08231
roman <roman@pushkeen.ru>
2020-07-19 00:01:24 +0300

    Решение

--- medium8-1-4.cs
+++ medium8-1-4.cs
@@ -1,16 +1,37 @@
 class Player
 {
     public string Name { get; private set; }
-    public float WeaponCooldown { get; private set; }
+    public int Age { get; private set; }
+
+    private Movement _movement = new Movement();
+    private Weapone _weapone = new Weapone();
+}
+
+class Movement
+{
     public float MovementDirectionX { get; private set; }
     public float MovementDirectionY { get; private set; }
-    public int Age { get; private set; }
-    public int WeaponDamage { get; private set; }
     public float MovementSpeed { get; private set; }
+
+    public Movement()
+    {
+
+    }
 
     public void Move()
     {
         //Do move
+    }
+}
+
+class Weapone
+{
+    public float WeaponCooldown { get; private set; }
+    public int WeaponDamage { get; private set; }
+
+    public Weapone()
+    {
+
     }
 
     public void Attack()
